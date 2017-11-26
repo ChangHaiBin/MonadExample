ba54a6ed3d77e2c109acdaaf923c8946ab5997ec
haibin chang <haibinmath@gmail.com>
2017-11-26 17:59:12 +0800

    Added example first fsharp file

---
+++ MonadDemonstration/Variation0.fs
@@ -0,0 +1,16 @@
+﻿module Variation0
+
+type Term = 
+    | Con of int
+    | Div of Term * Term
+
+module Term =
+    let rec eval0 = function
+        | Con x -> x
+        | Div (x, y) -> (eval0 x) / (eval0 y)
+
+let answer = (Div (Div (Con 1972, Con 2),Con 23)) 
+let error = Div (Con 1, Con 0)
+
+let example_0_1 = Term.eval0 answer
+let example_0_2 = Term.eval0 error
---
+++ MonadDemonstration/Variation1.fs
@@ -0,0 +1,27 @@
+﻿module Variation1
+
+open Variation0
+
+type Monad1<'a> =
+    | Raise of string
+    | ReturnValue of 'a
+
+module Monad1 =
+    let rec eval1_original = function
+        | Con a -> ReturnValue a
+        | Div (t,u) -> 
+            match eval1_original t with
+            | Raise e -> Raise e
+            | ReturnValue a ->
+                match eval1_original u with
+                | Raise e -> Raise e
+                | ReturnValue b ->
+                    if b = 0
+                        then Raise "divide by zero"
+                        else ReturnValue (a/b)
+
+let answer = (Div (Div (Con 1972, Con 2),Con 23)) 
+let error = Div (Con 1, Con 0)
+
+let Example_1_1 = Monad1.eval1_original answer
+let Example_1_2 = Monad1.eval1_original error
