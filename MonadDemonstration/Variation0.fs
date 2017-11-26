9c1feb7837dcbb7b05c13de848c9e521fd2213a8
haibin chang <haibinmath@gmail.com>
2017-11-26 18:28:37 +0800

    Monadic evaluator for example1

--- MonadDemonstration/Variation0.fs
+++ MonadDemonstration/Variation0.fs
@@ -9,8 +9,25 @@ module Term =
         | Con x -> x
         | Div (x, y) -> (eval0 x) / (eval0 y)
 
+type Monad0<'a> = 'a
+
+module Monad0 =
+    let lift a = a
+    let bind binder m0a =
+        binder m0a
+    let inline (>>=) m0a binder =
+        binder m0a
+
+    let rec monadicEval0 term =
+        match term with
+        | Con a -> lift a
+        | Div (t,u) -> 
+            (monadicEval0 t) >>= fun a ->
+            (monadicEval0 u) >>= fun b ->
+            lift (a / b)
+
 let answer = (Div (Div (Con 1972, Con 2),Con 23)) 
 let error = Div (Con 1, Con 0)
 
 let example_0_1 = Term.eval0 answer
-let example_0_2 = Term.eval0 error
+//let example_0_2 = Term.eval0 error
--- MonadDemonstration/Variation1.fs
+++ MonadDemonstration/Variation1.fs
@@ -20,8 +20,34 @@ module Monad1 =
                         then Raise "divide by zero"
                         else ReturnValue (a/b)
 
+    let lift a = ReturnValue a
+    let bind binder m1a =
+        match m1a with
+        | Raise e -> Raise e
+        | ReturnValue a -> binder a
+    let inline (>>=) m1a binder =
+        bind binder m1a
+    let raise exceptionString =
+        Raise exceptionString
+
+      
+    let rec monadicEval1 term =
+        match term with
+        | Con a -> lift a
+        | Div (t,u) -> 
+            (monadicEval1 t) >>= fun a ->
+            (monadicEval1 u) >>= fun b ->
+            if b = 0 then Raise "divide by zero"
+            else lift (a/b)
+
+
+
+
 let answer = (Div (Div (Con 1972, Con 2),Con 23)) 
 let error = Div (Con 1, Con 0)
 
 let Example_1_1 = Monad1.eval1_original answer
 let Example_1_2 = Monad1.eval1_original error
+
+let Example_1_Monadic_1 = Monad1.monadicEval1 answer
+let Example_1_Monadic_2 = Monad1.monadicEval1 error
